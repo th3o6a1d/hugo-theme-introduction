88f6a62f20702bc49d09a320203ba3595134bad3
Jason Theobald <jtheo540@gmail.com>
2018-09-17 22:10:55 -0400

    Change http:// to https://

--- static/js/moment-timezone-with-data-2012-2022.js
+++ static/js/moment-timezone-with-data-2012-2022.js
@@ -551,7 +551,7 @@
 			if (this._z) {
 				moment.updateOffset(this);
 			} else {
-				logError("Moment Timezone has no data for " + name + ". See http://momentjs.com/timezone/docs/#/data-loading/.");
+				logError("Moment Timezone has no data for " + name + ". See https://momentjs.com/timezone/docs/#/data-loading/.");
 			}
 			return this;
 		}
